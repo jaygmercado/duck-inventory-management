b5c463e7e00fd1ad95b7c5b54faafe768ff03c95
Jay Mercado <jaymercado@Jays-MacBook-Pro.local>
2024-05-16 23:10:58 +0800

    Fetch products for online store page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { signIn, useSession } from 'next-auth/react';
 import Link from 'next/link';
+import notify from '@/utils/notify';
+import { ProductType } from '@/types/products';
+
+const loadProducts = async () => {
+  const res = await fetch('/api/products');
+  if (!res.ok) throw new Error('unable to fetch products');
+
+  return (await res.json().then((res) => res.data)) as ProductType[];
+};
 
 export default function App() {
   const session = useSession();
+  const [products, setProducts] = useState<ProductType[]>([]);
+  useEffect(() => {
+    loadProducts()
+      .then(setProducts)
+      .catch(() => notify('Error', 'Unable to retrieve products'));
+  }, []);
   return (
     <main className='h-full'>
       <h1 className='font-bold text-2xl mb-5'>Welcome</h1>
@@ -14,6 +29,9 @@ export default function App() {
       ) : (
         <button onClick={() => signIn()}>Sign In</button>
       )}
+      {products.map((product) => (
+        <div key={product._id}>{product.name}</div>
+      ))}
     </main>
   );
 }
