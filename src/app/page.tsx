cefb5bde72b6b7972aa8c0277104fedfec9a5dcb
Alexander Oxales <ajoxales@gmail.com>
2024-05-18 04:17:32 +0800

    removed searchbox on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,40 +103,6 @@ export default function App() {
                 <h1 className='font-bold' style={{ fontSize: 30 }}>
                   Browse Products
                 </h1>
-                <form className=''>
-                  <label
-                    htmlFor='default-search'
-                    className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white'
-                  >
-                    Search
-                  </label>
-                  <div className='relative'>
-                    <div className='absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none'>
-                      <svg
-                        className='w-4 h-4 text-gray-500 dark:text-gray-400'
-                        aria-hidden='true'
-                        xmlns='http://www.w3.org/2000/svg'
-                        fill='none'
-                        viewBox='0 0 20 20'
-                      >
-                        <path
-                          stroke='currentColor'
-                          stroke-linecap='round'
-                          stroke-linejoin='round'
-                          stroke-width='2'
-                          d='m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z'
-                        />
-                      </svg>
-                    </div>
-                    <input
-                      type='search'
-                      id='default-search'
-                      className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                      placeholder='Type a product name'
-                      required
-                    />
-                  </div>
-                </form>
               </div>
               <div className='mt-3 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
                 <Products products={products} setProductToPurchase={setProductToPurchase} />
